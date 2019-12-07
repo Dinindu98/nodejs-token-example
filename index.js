875efd98990261429cc938e03a0d8b21b550a9ea
Dinindu98 <dinindu.ayagama@gmail.com>
2019-12-06 18:53:50 -0800

    first commit

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const express = require('express'); // npm install express / npm install nodemon
 const app = express();
 const dotenv = require('dotenv');
 const mongoose = require('mongoose'); // npm install mongoose / npm install dotenv
+const authRoute = require('./routes/auth');
 
 dotenv.config();
 mongoose.connect(
-    process.env.DB_CONNECT,{useNewUrlParser:true},
-    ()=>console.log('connected to db'));
+    process.env.DB_CONNECT,{ useNewUrlParser: true , useUnifiedTopology: true},
+    ()=>console.log('connected to db'));  // connect with mongo db
 
-const authRoute = require('./routes/auth');
+ 
+
+app.use(express.json());
 app.use('/api/user',authRoute);
 app.listen(5000);
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,20 @@
 const router = require('express').Router();
+const User = require("../Modules/User");
 
-router.post('/register',function(req,res){
-    res.send('Register');
+//in here register means => in index.js api.use + authRoute
+router.post('/register',async(req,res)=>{  //async - wait for the until data writingto the database
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    });
+    try{
+        const saveUser = await user.save();
+        res.send(saveUser);
+    }catch(err){
+        res.status(400).send(err);
+        res.send(" this is fucking error");
+    }
 })
 
 module.exports = router;
